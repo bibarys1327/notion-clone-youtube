a10183245e79468b3be0d5550d98cc27722d8f28
Bibarys <bibaryssultan@icloud.com>
2024-11-22 20:23:53 +0500

    setup clerk shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-	return <div>Home</div>
+	return (
+		<main>
+			<h1>Lets build an API productivity App</h1>
+			<Button>Click Me</Button>
+		</main>
+	)
 }
